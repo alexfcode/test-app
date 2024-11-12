9812f0c91e2220ae7093c8c0b18bdc41abf2cdf3
alexf <alexf@tut.by>
2024-11-12 09:44:19 +0300

    continue

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function Hello() {
+  debugger;
+  alert("Hello, niga!!!");
+}
+
+// Hello()
 
 function App() {
+  debugger;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          
-        </a>
-      </header>
+    <div>
+      This is app component
+      <Rating />
+      <Accordion />
+    </div>
+  );
+}
+
+function Star() {
+  return (
+    <div>*</div>
+  )
+}
+
+function Rating() {
+  debugger;
+  return (
+    <div>
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+    </div>
+  );
+}
+
+function Accordion() {
+  debugger;
+  return (
+    <div>
+      <h3>Меню</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
     </div>
   );
 }
